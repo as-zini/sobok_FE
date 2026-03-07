f6f17e029a14101056490e028c748bb7806a92b1
황지현 <aszini@hwangjihyeon-ui-MacBookAir.local>
2026-03-07 13:43:21 +0900

    chore: 폰트 파일 추가

--- src/common/ui/screens/StartScreen.tsx
+++ src/common/ui/screens/StartScreen.tsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/native';
 import { colors } from '@/common/ui/styles/colors';
 import StartScreenModal from '@/common/ui/components/StartScreenModal';
-import { useNavigation } from '@react-navigation/native';
 import { size } from '@/common/ui/styles/size';
+import bg from '@/assets/start_bg.png';
 
 const StartScreen = () => {
   const [isSignupModalVisible, setIsSignupModalVisible] = useState(false);
-  const navigation = useNavigation();
-
-  useEffect(() => { }, []);
 
   return (
     <StartScreenBody>
-      <StartScreenBg
-        source={require('@/assets/start_bg.png')}
-      />
-      <StartText style={{ fontFamily: 'Pretendard ' }}>
-        안녕하세요{`\n`}환영합니다
-      </StartText>
+      <StartScreenBg source={bg} />
+      <StartText style={{ fontFamily: 'Pretendard ' }}>안녕하세요{`\n`}환영합니다</StartText>
       <StartButton onPress={() => setIsSignupModalVisible(true)}>
         <StartButtonText>시작하기</StartButtonText>
       </StartButton>
@@ -32,25 +25,22 @@ const StartScreen = () => {
 
 export default StartScreen;
 
-// Main container should fill screen to allow bg to size correctly
 const StartScreenBody = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  width:${() => `${size.width}px`};
-  height:${() => `${size.height}px`};
+  width: ${() => `${size.width}px`};
+  height: ${() => `${size.height}px`};
 `;
 
-// Background image placed absolutely with pointerEvents none to let touches pass
 const StartScreenBg = styled.Image`
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  z-index:-1;
+  z-index: -1;
 `;
-
 
 const StartText = styled.Text`
   position: absolute;
@@ -58,7 +48,6 @@ const StartText = styled.Text`
   bottom: 150px;
   font-weight: 600;
   color: ${colors.fontMain};
-  
 `;
 
 const StartButton = styled.TouchableOpacity`
