6ef3e97c494aa0f1909713b07b6a0a3fbaed528f
황지현 <aszini@hwangjihyeon-ui-MacBookAir.local>
2026-03-08 11:36:18 +0900

    feat: 기본 텍스트 컴포넌트 생성

---
+++ src/common/ui/components/DefaultText.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/native';
+import { colors } from '../styles/colors';
+
+export const DefaltText = styled.Text({
+  fontFamily: 'Pretendard-Medium',
+  color: colors.fontMain,
+});
